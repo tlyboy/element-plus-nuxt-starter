97223d7d0df995d761917e7919f420f29373acd1
Guany <1084072690@qq.com>
2024-03-01 22:03:55 +0800

    chore: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: [
-    '@unocss/reset/tailwind.css',
-    '~/assets/styles/main.css',
-    '~/assets/styles/index.scss',
-  ],
+  srcDir: 'src/',
+  css: ['~/assets/styles/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   modules: [
     '@vue-macros/nuxt',
     '@nuxtjs/color-mode',
-    '@unocss/nuxt',
     '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
     '@vueuse/nuxt',
@@ -19,7 +21,7 @@ export default defineNuxtConfig({
     classSuffix: '',
   },
   imports: {
-    dirs: ['stores'],
+    dirs: ['src/stores'],
   },
   vite: {
     css: {
--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import { appName } from '~/constants'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 useHead({
-  htmlAttrs: {
-    lang: 'zh-CN',
-  },
   title: appName,
 })
 
