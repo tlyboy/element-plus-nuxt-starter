9d527a95e8ea9344599ab0cabadb3055d6b1c5a9
Guany <1084072690@qq.com>
2025-09-13 16:48:33 +0800

    更新项目名称为 Element Plus Nuxt Starter，修改相关链接

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,7 +5,7 @@
     <div class="flex items-center gap-2 text-xl">
       <a
         class="i-carbon-logo-github icon-btn"
-        href="https://github.com/tlyboy/nuxt-starter"
+        href="https://github.com/tlyboy/element-plus-nuxt-starter"
         target="_blank"
         rel="noopener noreferrer"
       ></a>
